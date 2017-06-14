2ce475114a16278cdbe6e55b40a6ca1233689ecf
sindrsh <sindrsh@student.matnat.uio.no>
2017-06-14 10:34:02 +0200

    Create menuscript0.js

---
+++ menuscript0.js
@@ -0,0 +1,6 @@
+ <script>
+        var link = document.querySelector('link[rel="import"]');
+        var content = link.import;
+        var el = content.querySelector('.menu');
+        document.body.appendChild(el.cloneNode(true));
+      </script>
