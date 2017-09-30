a4cd1d6465880b1bdff3455cf7c9dd75a86eeb13
sindrsh <sindrsh@student.matnat.uio.no>
2017-09-30 11:10:44 +0200

    Create homescr.js

---
+++ homescr.js
@@ -0,0 +1,19 @@
+var burl = "http://forkalkulus.netlify.com/";
+var scrw = 500;
+
+if (window.innerWidth < 500){
+  window.location.assign(window.location.href+"m")}
+
+(function() {
+  Array.prototype.forEach.call(document.querySelectorAll("a.root"), function(link) {
+    link.href = burl + link.getAttribute("data-extra");
+  });
+})();
+
+$(document).ready(function() {
+    $("[href]").each(function() {
+    if (this.href == window.location.href) {
+        $(this).addClass("active");
+        }
+    });
+});   
