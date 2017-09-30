653dfe2bb841db5d7c6765f98cda821656a532e1
sindrsh <sindrsh@student.matnat.uio.no>
2017-09-30 10:58:50 +0200

    Update menuscript.js

--- menuscript.js
+++ menuscript.js
@@ -1,6 +1,9 @@
 var burl = "http://forkalkulus.netlify.com/";
 var scrw = 500;
 
+if (window.innerWidth < 500){
+  window.location.assign("#m")}
+
 (function() {
   Array.prototype.forEach.call(document.querySelectorAll("a.root"), function(link) {
     link.href = burl + link.getAttribute("data-extra");
