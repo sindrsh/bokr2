435dcec563af07343ec603a05d5356e4b9f72200
sindrsh <sindrsh@student.matnat.uio.no>
2017-09-30 11:18:14 +0200

    Update homescr.js

--- homescr.js
+++ homescr.js
@@ -1,9 +1,6 @@
 var burl = "https://forkalkulus.netlify.com/";
 var scrw = 500;
 
-if (window.innerWidth < 500){
-  window.location.assign("https://forkalkulus.netlify.com/homem")}
-
 (function() {
   Array.prototype.forEach.call(document.querySelectorAll("a.root"), function(link) {
     link.href = burl + link.getAttribute("data-extra");
