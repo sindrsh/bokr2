675ee726e004cdd91fa63e6b26632acfc1223725
sindrsh <sindrsh@student.matnat.uio.no>
2017-10-01 07:41:18 +0200

    Update menuscript.js

--- menuscript.js
+++ menuscript.js
@@ -4,12 +4,6 @@ var scrw = 500;
 if (window.innerWidth < 500){
   window.location.assign(window.location.href+"m")}
 
-(function() {
-  Array.prototype.forEach.call(document.querySelectorAll("a.root"), function(link) {
-    link.href = burl + link.getAttribute("data-extra");
-  });
-})();
-
 $(document).ready(function() {
     $("[href]").each(function() {
     if (this.href == window.location.href) {
