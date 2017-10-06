e8496a3914abdccd7c314f3152ad28f0665ae67e
sindrsh <sindrsh@student.matnat.uio.no>
2017-10-06 15:21:37 +0200

    Update menumscript.js

--- menumscript.js
+++ menumscript.js
@@ -1,17 +1,5 @@
-var cnt = 0;    
-function openNav() {
-    if (cnt ==0){
-        document.getElementById("mySidenav").style.height = "660px";
-        document.getElementById("main").style.marginTop = "660px";
-        cnt = 1;
-    }
-    else{
-        document.getElementById("mySidenav").style.height = "52px";
-        document.getElementById("main").style.marginTop = "52px";
-        cnt = 0;
-    }
-}
-function closeNav() {
-    document.getElementById("mySidenav").style.height = "0";
-    document.getElementById("main").style.marginTop= "0";
-}
+
+var link = document.querySelector('link[rel="import"]');
+var content = link.import;
+var el = content.querySelector('.menu');
+document.body.appendChild(el.cloneNode(true));
