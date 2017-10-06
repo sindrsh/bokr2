ac330b8b06e30cd1ce71f06f1225b4486de8ce1d
sindrsh <sindrsh@student.matnat.uio.no>
2017-10-06 15:22:40 +0200

    Update menumscript0.js

--- menumscript0.js
+++ menumscript0.js
@@ -1,4 +1,4 @@
 var link = document.querySelector('link[rel="import"]');
 var content = link.import;
-var el = content.querySelector('.menu');
+var el = content.querySelector('.topnav');
 document.body.appendChild(el.cloneNode(true));
