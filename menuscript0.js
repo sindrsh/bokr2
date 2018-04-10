9c668367a1bfbfca14a88ad8f8161cdeeadd58f1
sindrsh <sindrsh@student.matnat.uio.no>
2018-04-11 00:33:16 +0200

    Update menuscript0.js

--- menuscript0.js
+++ menuscript0.js
@@ -2,5 +2,3 @@ var link = document.querySelector('link[rel="import"]');
 var content = link.import;
 var el = content.querySelector('.menu');
 document.body.appendChild(el.cloneNode(true));
-var e2 = content.querySelector('.menub');
-document.body.appendChild(e2.cloneNode(true));
