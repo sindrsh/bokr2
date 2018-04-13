b5fb0020e0f5448979f42c8d11ba51cce9dfeda1
sindrsh <sindrsh@student.matnat.uio.no>
2018-04-13 19:14:10 +0200

    Update menuscript.js

--- menuscript.js
+++ menuscript.js
@@ -2,12 +2,4 @@ var hmp = "http://forkalkulus.netlify.com/";
 var scrw = 700;
 
 if (window.innerWidth < 700){
-  window.location.assign(window.location.href+"m")}
-
-$(document).ready(function() {
-    $("[href]").each(function() {
-    if (this.href == window.location.href) {
-        $(this).addClass("active");
-        }
-    });
-});   
+  window.location.assign(window.location.href+"m")} 
